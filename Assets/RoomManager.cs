3ab9945657c4e8d677bde116f89f4ffa12b29532
Muhammad Ashhad <betadrone596@gmail.com>
2023-12-18 00:09:19 -0500

    Updated move and roomManager

--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
     public GameObject player;
-    [Space]
-    public GameObject spawnPoint;
+    private float minX, maxX, minZ, maxZ;
 
     void Start()
     {
@@ -41,6 +41,13 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
         Debug.Log("Joined Room");
 
-        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.transform.position, Quaternion.identity);
+        minX = -5.0f;
+        minZ = -5.0f;
+        maxX = 5.0f;
+        maxZ = 5.0f;
+
+        Vector3 randSpawnPoint = new Vector3(UnityEngine.Random.Range(minX, maxX), 0.35f, UnityEngine.Random.Range(minZ, maxZ));
+
+        GameObject _player = PhotonNetwork.Instantiate(player.name, randSpawnPoint, Quaternion.identity);
     }
 }
